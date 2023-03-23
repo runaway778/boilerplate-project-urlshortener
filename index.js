7be7159d456a5ddeec42cf4b6a254860d152b06b
runaway778 <batyrov-amir@bk.ru>
2023-03-23 14:48:50 +0300

    Challenge complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
@@ -10,6 +12,8 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -17,6 +21,25 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+let urls = [];
+
+app.post('/api/shorturl', (req, res) => {
+  dns.lookup(new URL(req.body.url).hostname, (err, address, family) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      if (!urls.includes(req.body.url)) {
+        urls.push(req.body.url);
+      }
+      res.json({ original_url: req.body.url, short_url: urls.indexOf(req.body.url) });
+    }
+  });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  res.redirect(urls[req.params.short_url]);
 });
 
 app.listen(port, function() {
